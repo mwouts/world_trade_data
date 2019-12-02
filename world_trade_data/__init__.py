796ec85f9b09db3785f2ca8bde778056e1630522
Marc Wouts <marc.wouts@gmail.com>
2019-12-02 01:04:47 +0100

    Add __version__

--- world_trade_data/__init__.py
+++ world_trade_data/__init__.py
@@ -2,6 +2,7 @@
 
 from .referential import get_countries, get_nomenclatures, get_products, get_dataavailability, get_indicators
 from .data import get_indicator, get_tariff_reported, get_tariff_estimated, DATASOURCES
+from .version import __version__
 
 __all__ = ['get_countries', 'get_nomenclatures', 'get_products', 'get_dataavailability', 'get_indicators',
-           'get_indicator', 'get_tariff_reported', 'get_tariff_estimated', 'DATASOURCES']
+           'get_indicator', 'get_tariff_reported', 'get_tariff_estimated', 'DATASOURCES', '__version__']
