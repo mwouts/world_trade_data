684fa601eb627e8157e45420f0fe349c330fa7a9
Marc Wouts <marc.wouts@gmail.com>
2020-04-05 20:22:59 +0200

    More tests

--- tests/test_get_indicator.py
+++ tests/test_get_indicator.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 import pandas as pd
 from world_trade_data import get_indicator, get_indicators
 
@@ -10,8 +11,34 @@ def test_get_indicators():
     assert 'TRD-CMPLMNTRTY-NDX' in df.index
 
 
-@pytest.mark.parametrize('indicator', ['MPRT-TRD-VL', 'XPRT-TRD-VL', 'WRLD_GRWTH', 'TRD-CMPLMNTRTY-NDX'])
+@pytest.mark.parametrize('indicator', ['MPRT-TRD-VL', 'XPRT-TRD-VL', 'WRLD-GRWTH'])
 def test_get_indicator(indicator):
     df = get_indicator(indicator, 'usa')
+    assert isinstance(df, pd.DataFrame)
+    assert len(df.index)
+
+
+def test_error_on_wrong_indicator(indicator='INCORRECT'):
+    with pytest.raises(requests.exceptions.RequestException, match='http://wits.worldbank.org'):
+        get_indicator(indicator, 'usa')
+
+
+def test_error_on_wrong_partner(indicator='NMBR-XPRT-PRTNR'):
+    with pytest.raises(requests.exceptions.RequestException, match='http://wits.worldbank.org'):
+        get_indicator(indicator, 'usa')
+
+
+@pytest.mark.parametrize('indicator', get_indicators().query('SDMX_partnervalue==""').index)
+def test_get_all_indicators(indicator):
+    if indicator == 'TRD-CMPLMNTRTY-NDX':
+        pytest.xfail('TRD-CMPLMNTRTY-NDX is not available - cf. https://github.com/mwouts/world_trade_data/issues/1')
+    df = get_indicator(indicator, 'usa')
+    assert isinstance(df, pd.DataFrame)
+    assert len(df.index)
+
+
+@pytest.mark.parametrize('indicator', get_indicators().query('SDMX_partnervalue=="999"').index)
+def test_get_all_indicators_no_partner(indicator):
+    df = get_indicator(indicator, 'usa', partner='999')
     assert isinstance(df, pd.DataFrame)
     assert len(df.index)
