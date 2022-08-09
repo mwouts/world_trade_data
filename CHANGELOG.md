5b59780deb3b3c1d0dfa086954dbc9961a0ed5f2
Marc Wouts <marc.wouts@gmail.com>
2022-08-09 17:18:42 +0200

    Test with Python 3.6 to 3.10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@
 **Fixed**
 - Fixed an IndexError when calling `wits.get_tariff_reported` ([#3](https://github.com/mwouts/world_trade_data/issues/3))
 
+**Changed**
+- Versions of Python supported are 3.6 to 3.10.
+
 
 0.1.0 (2019-11-25)
 ==================
--- setup.py
+++ setup.py
@@ -34,10 +34,9 @@ setup(
                  'Intended Audience :: Education',
                  'Intended Audience :: Science/Research',
                  'Programming Language :: Python',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3.7',
-                 'Programming Language :: Python :: 3.8']
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10']
 )
